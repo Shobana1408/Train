44c7114290f4ebd151f14274d37b8779178d53b7
Shobanasri G <shobanasrisrmcse@gmail.com>
2026-04-15 20:29:00 +0530

    Create UseCase10TrainConsistMgmt .java

---
+++ UseCase10TrainConsistMgmt .java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+class UseCase10TrainConsistMgmt {
+    static class Bogie {
+        String name;
+        int capacity;
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    }
+    public static void main(String[] args) {
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Total Seating Capacity: " + totalSeats);
+    }
+}
